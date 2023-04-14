3ff241de70984585b331ad349832ed1a87354aa0
Francisco Forciniti <93131373+FranciscoForciniti@users.noreply.github.com>
2023-04-14 15:35:41 -0300

    prueba contacto

---
+++ PHP/correos.php
@@ -0,0 +1,15 @@
+<?php
+    $destinatario = 'franciscoforcii@gmail.com';
+    // esto es al correo que resive el mensaje
+    $nombre = $_POST['nombre'];
+    $asunto = $_POST['asunto'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+
+    $header = "enviado desde la pagina de contacto de GMTech";
+    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
+    echo"<script>alert('Correo enviado exitosamente')</script>";
+echo"<script>setTimeout(\"location.href='../index.html'\", 1000)</script>";
+?>
